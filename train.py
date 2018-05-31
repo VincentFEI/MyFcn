ef84ab0cd21b5faf471654e1e6bd818ee34f45b8
VincentFEI <852234496@qq.com>
2018-05-31 13:17:56 +0800

    20170531

--- train.py
+++ train.py
@@ -147,8 +147,8 @@ def main(argv=None):
     # 开启一个新会话
     with tf.Session() as sess:
 
-        # 初始化Saver，用于存储模型参数
-        saver = tf.train.Saver()
+        # 初始化Saver，用于存储模型参数，最多记录10个模型
+        saver = tf.train.Saver(max_to_keep=11)
         # 初始化Summary_writer，用于记录summary
         summary_writer = tf.summary.FileWriter(LOGS_DIR, sess.graph)
         # 初始化所有参数
@@ -175,7 +175,7 @@ def main(argv=None):
                     train_loss, summary_str = sess.run([loss, summary_op], feed_dict=feed_dict)
                     print("Step: %d, Train_loss:%g" % (itr, train_loss))
                     summary_writer.add_summary(summary_str, itr)
-                # 500次训练后，计算一次验证集上的loss，储存模型
+                # 500次训练后，计算一次验证集上的loss
                 if itr % 500 == 0:
                     valid_images, valid_annotations = val_dataset.get_next_batch(BATCH_SIZE)
                     # valid_images = np.reshape(valid_images, [-1, IMAGE_WIDTH, IMAGE_HEIGHT, 3])
@@ -183,6 +183,8 @@ def main(argv=None):
                     feed_dict = {images: valid_images, annotation: valid_annotations, keep_probability: 1.0}
                     valid_loss = sess.run(loss, feed_dict=feed_dict)
                     print("%s ---> Validation_loss: %g" % (datetime.datetime.now(), valid_loss))
+                # 5000次训练后，记录模型参数
+                if itr % 5000 == 0:
                     saver.save(sess, LOGS_DIR + "model.ckpt", itr)
 
         elif MODE == "test":

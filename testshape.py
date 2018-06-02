50532d9d2a72682a7099a3d93ffddfb58764d7b7
VincentFEI <852234496@qq.com>
2018-06-02 11:09:17 +0800

    20170531

--- testshape.py
+++ testshape.py
@@ -1,3 +1,4 @@
+import numpy as np
 from utils import BatchDatsetReader
 from utils import ReadMITSceneParing
 train_records, valid_records = ReadMITSceneParing.read_dataset("/DATA/234/gxrao1/DeepLearning/FCN.tensorflow/Data_zoo/MIT_SceneParsing")
@@ -7,5 +8,7 @@ train_dataset = BatchDatsetReader.BatchDatset(train_records[:10], image_options)
 train_images, train_annotations = train_dataset.get_next_batch(2)
 print(train_images.shape)
 print(train_annotations.shape)
+train_new_images = np.asarray(train_images)
+print(train_new_images.shape)
 print(train_images)
 print(train_annotations)
